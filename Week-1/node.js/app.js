a7fd6e614cf303dfe6886457c9ba1a4147ab7f7f
unknown <rdogan8891gmail.com>
2022-02-15 19:09:40 +0100

    comment has been added

--- Week-1/node.js/app.js
+++ Week-1/node.js/app.js
@@ -1,5 +1,12 @@
-let calculator = require('./calculator');
+/*
+1-Calculator.js dosyasinda fonksiyonlar olusturludu.
+2-Moduller expoert edildi.
+3-Require yailarak app.js dosyasinda import edildi.
+4-x ve y degiskenlerine deger verilerek console.log da yazdirildi.
+*/
 
+let calculator = require('./calculator');
+//x ve y degiskenlerine deger verilerek fonksiyonlar calistirildi.
 let x = 50, y = 20;
 
 console.log("Addition of 50 and 20 is "
