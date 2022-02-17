dbeae42af4c16debab74b2c4d1faba3ce92b2086
unknown <rdogan8891gmail.com>
2022-02-17 15:58:10 +0100

    first react has been added

---
+++ Week-1/first-react.js/src/components/cards.js
@@ -0,0 +1,13 @@
+function Cards(){
+    return (
+      <div class="card col-3 mt-5 ms-5 me-5">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Logo_of_Be%C5%9Fikta%C5%9F_JK.svg/1200px-Logo_of_Be%C5%9Fikta%C5%9F_JK.svg.png" class="card-img-top w-75 ms-5" alt="Besiktas"/>
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+    )    
+}
+
+export default Cards;
---
+++ Week-1/first-react.js/src/components/footer.js
@@ -0,0 +1,9 @@
+function Footer(){
+   return(
+<div class="row w-100 bg-secondary mt-5 p-2">
+    <div class="text-center ">Copyright<i class="fa-thin fa-copyright"></i> 2022</div>
+</div>
+   ) 
+}
+
+export default Footer;
