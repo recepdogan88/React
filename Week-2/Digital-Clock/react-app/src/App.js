ad3037428c3a5766d26f571ff35cc1418bd9372c
unknown <rdogan8891gmail.com>
2022-02-22 23:10:08 +0100

    digital clock has been added

---
+++ Week-2/Digital-Clock/react-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './MyComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>React Clock</h2>
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
