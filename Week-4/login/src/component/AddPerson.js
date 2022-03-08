34884b13cfc895c437c785ea85cc8aeb784a8fb8
unknown <rdogan8891gmail.com>
2022-03-08 20:03:08 +0100

    comments have been added

--- Week-4/login/src/component/AddPerson.js
+++ Week-4/login/src/component/AddPerson.js
@@ -18,24 +18,25 @@ function handleClick(e){
         mail:mail,
         password:password
      }
-     if(password==passcheck&&mail==regEx){
+     if(password==passcheck){
         addPerson(newPerson)
-     }else if(mail!==regEx){
-      alert("Gecersiz mail")
      }
-      else {
-        alert("Passwordunuz yanlis girildi")
+     else if(password!==passcheck){
+       alert("Sifre uyusmadi")
+     }
+    else {
+        alert("Login isleminiz basariyla tamamlandi")
     }
 }
   return (
     <div>
         <form className="d-flex flex-column ">
             <h2 className="text-primary">SIGN UP</h2>
-            <input type="text"className="form-control mt-3"   value={userName} onChange={(e) =>setuserName(e.target.value)} required />
-            <input type="mail" className="form-control mt-4" value={mail} onChange={(e) =>setmail(e.target.value)}/>
+            <input type="text"className="form-control mt-3" placeholder="Username"  value={userName} onChange={(e) =>setuserName(e.target.value)} required />
+            <input type="mail" className="form-control mt-4"placeholder="Email" value={mail} onChange={(e) =>setmail(e.target.value)}/>
             <span className="text-danger mt-2">{message}</span>
-            <input type="password" className="form-control mt-4" value={password} onChange={(e) =>setpassword(e.target.value)}/>
-            <input type="password" className="form-control mt-4" value={passcheck} onChange={(e) =>setpasscheck(e.target.value)}/>
+            <input type="password" placeholder="Password" className="form-control mt-4" value={password} onChange={(e) =>setpassword(e.target.value)}/>
+            <input type="password" placeholder="Password Check" className="form-control mt-4" value={passcheck} onChange={(e) =>setpasscheck(e.target.value)}/>
             
         </form>
         <button type="submit" onClick={handleClick} className="mt-5 btn btn-primary btn-lg" >Sign up</button>
